e53937ddf655b64e72fae4c25166e2c148b63bec
Edmondo Silvestri <Edmondo.Silvestri@ptvgroup.com>
2023-02-06 09:14:58 +0100

    read ephemerides

--- EpemeridesReader/EphemeridesReader.cs
+++ EpemeridesReader/EphemeridesReader.cs
@@ -26,7 +26,8 @@ namespace EpemeridesReader
       var count = blockHeader[1];
 
       var values = streamReader.ReadDoubles(count).ToArray();
-
+      var valuesSpan = new Span<double>(values);
+      var currentIndex = 2;
       for (var currentSeries = 0;
         currentSeries < header.SeriesDescriptions.Length;
         currentSeries++) {
@@ -37,30 +38,27 @@ namespace EpemeridesReader
           count) - seriesDescription.StartOffset;
         var chebCoefficientIntervals = new ChebCoefficientsInterval[seriesDescription.NumberOfIntervals];
         var numberOfProperties =
-          currentSeriesLength /
-          seriesDescription.NumberOfCoefficients /
-          seriesDescription.NumberOfIntervals;
+          seriesDescription.NumberOfCoefficients > 0 && 
+          seriesDescription.NumberOfIntervals > 0 ?
+            currentSeriesLength /
+            seriesDescription.NumberOfCoefficients /
+            seriesDescription.NumberOfIntervals:
+            0;
         for (var interval = 0; interval < seriesDescription.NumberOfIntervals; interval++) {
           var chebCoefficientInterval = new ChebCoefficientsInterval();
           chebCoefficientInterval.ChebCoefficients = new ChebCoefficients[numberOfProperties];
           for (var property = 0; property < numberOfProperties; property++) {
             var chebCoefficients = new ChebCoefficients {
-              //Coefficients = 
+              Coefficients = valuesSpan.Slice(currentIndex, seriesDescription.NumberOfCoefficients).ToArray()
             };
+            chebCoefficientInterval.ChebCoefficients[property] = chebCoefficients;
+            currentIndex += seriesDescription.NumberOfCoefficients;
           }
           chebCoefficientIntervals[interval] = chebCoefficientInterval;
         }
-        block.ChebCoefficientSeries[currentSeries].ChebCoefficientIntervals = chebCoefficientIntervals;
-      }
-
-      while (count > 0) {
-        var row = streamReader.
-        ReadNonEmptyLine().
-        SplitInternal().
-        Select(StringExtensions.ToDouble).
-        ToArray();
-        count -= row.Length;
-
+        block.ChebCoefficientSeries[currentSeries] = new ChebCoefficientsSeries {
+          ChebCoefficientIntervals = chebCoefficientIntervals
+        };
       }
       return block;
     }
